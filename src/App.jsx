ecad09bd40070f949cce48a5b8ec7c343d350ed0
FernandoLorca <florcapinto@gmail.com>
2023-01-10 22:12:13 -0300

    add workes functionality done

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,33 @@
-import { BaseColaboradores } from "./baseColaboradores";
+import { useState } from "react";
+import { BaseColaboradores as db } from "./baseColaboradores";
 
 const App = () => {
+  const [workers, setWorkers] = useState(db);
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+
+  const inputNameChange = (e) => {
+    setName(e.target.value);
+  };
+
+  const inputEmailChange = (e) => {
+    setEmail(e.target.value);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    const newWorker = {
+      id: Date.now(),
+      nombre: name,
+      correo: email,
+    };
+
+    setWorkers([...workers, newWorker]);
+    setName("");
+    setEmail("");
+  };
+
   return (
     <>
       <div className="flex justify-between items-center bg-gray-900 text-white p-4">
@@ -8,25 +35,29 @@ const App = () => {
         <input
           type="text"
           placeholder="Busca un colaborador"
-          className="rounded p-1"
+          className="text-gray-700 rounded p-1"
         />
       </div>
 
-      <form className="p-5">
+      <form onSubmit={handleSubmit} className="p-5">
         <div className="mb-5">
           <p className="mb-2">Nombre del colaborador</p>
           <input
             type="text"
-            className="border border-gray-600 py-2 px-3 w-full rounded"
+            className="border border-gray-600 py-2 px-3 w-full text-gray-700 rounded"
             placeholder="Ingresa el nombre del colaborador"
+            value={name}
+            onChange={inputNameChange}
           />
         </div>
         <div className="mb-5">
           <p className="mb-2">Correo del colaborador</p>
           <input
             type="text"
-            className="border border-gray-600 py-2 px-3 w-full rounded"
+            className="border border-gray-600 py-2 px-3 w-full text-gray-700 rounded"
             placeholder="Ingresa el correo del colaborador"
+            value={email}
+            onChange={inputEmailChange}
           />
         </div>
         <button
@@ -40,8 +71,8 @@ const App = () => {
       <div className="p-5">
         <h2 className="text-2xl mb-3">Listado de colaboradores</h2>
         <ul>
-          {BaseColaboradores.map((worker) => (
-            <li>
+          {workers.map((worker) => (
+            <li key={worker.id}>
               {worker.nombre} - {worker.correo}
             </li>
           ))}
